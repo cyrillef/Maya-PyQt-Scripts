a8ad9891578bf371a56d8120a8b36d03a4fb7cdb
vijayaprakash <vijaya.prakash@autodesk.com>
2016-10-25 14:47:05 +0530

    move test file for pyqt5 under test directory

---
+++ tests/pyqt5-test.py
@@ -0,0 +1,32 @@
+import sys
+from PyQt5.QtWidgets import (QWidget, QToolTip, QPushButton)
+from PyQt5.QtGui import QFont    
+
+
+class Example(QWidget):
+    
+    def __init__(self):
+        super(Example,self).__init__()
+        
+        self.initUI()
+        
+        
+    def initUI(self):
+        
+        QToolTip.setFont(QFont('SansSerif', 10))
+        
+        self.setToolTip('This is a <b>QWidget</b> widget')
+        
+        btn = QPushButton('Button', self)
+        btn.setToolTip('This is a <b>QPushButton</b> widget')
+        btn.resize(btn.sizeHint())
+        btn.move(50, 50)       
+        
+        self.setGeometry(300, 300, 300, 200)
+        self.setWindowTitle('Tooltips')    
+        self.show()
+        
+
+   
+ex = Example()
+   
