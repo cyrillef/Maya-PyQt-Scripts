f4265f3de939f048a5d00954826041029148d3d1
cxli <sos1096@hotmail.com>
2019-01-15 13:28:33 +0800

    Fix linux script and updating macosx/windows scripts.
    Fix linux script and updating macosx/windows scripts.

--- win/sip.bat
+++ win/sip.bat
@@ -4,7 +4,7 @@ if ["%MAYAPYQTBUILD%"]==[""] call "%XXX%setup.bat"
 
 pushd %SIPDIR%
 rem "%MAYA_LOCATION%\bin\mayapy" configure-ng.py --spec %_QMAKESPEC_%
-"%MAYA_LOCATION%\bin\mayapy" configure.py
+"%MAYA_LOCATION%\bin\mayapy" configure.py --sip-module PyQt5.sip 
 nmake
 nmake install
 popd
